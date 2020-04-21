5f38fe9cc84774458126df320bed362d020ace5e
Ajay Nair <AJ4942BO@go.minnstate.edu>
2020-04-21 15:59:09 -0500

    Initial Commit.

--- Weight on Mars/Form1.Designer.cs
+++ Weight on Mars/Form1.Designer.cs
@@ -30,12 +30,15 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.txtWeightEarth = new System.Windows.Forms.TextBox();
+            this.txtWeightMars = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(129, 112);
+            this.label1.Location = new System.Drawing.Point(77, 66);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(166, 25);
             this.label1.TabIndex = 0;
@@ -44,21 +47,47 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(107, 208);
+            this.label2.Location = new System.Drawing.Point(77, 154);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(163, 25);
             this.label2.TabIndex = 1;
             this.label2.Text = "Weight on Mars";
             // 
+            // txtWeightEarth
+            // 
+            this.txtWeightEarth.Location = new System.Drawing.Point(291, 66);
+            this.txtWeightEarth.Name = "txtWeightEarth";
+            this.txtWeightEarth.Size = new System.Drawing.Size(144, 31);
+            this.txtWeightEarth.TabIndex = 2;
+            // 
+            // txtWeightMars
+            // 
+            this.txtWeightMars.Location = new System.Drawing.Point(291, 154);
+            this.txtWeightMars.Name = "txtWeightMars";
+            this.txtWeightMars.Size = new System.Drawing.Size(144, 31);
+            this.txtWeightMars.TabIndex = 3;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(173, 220);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(172, 60);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(562, 304);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtWeightMars);
+            this.Controls.Add(this.txtWeightEarth);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Mars Weight";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -68,6 +97,9 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtWeightEarth;
+        private System.Windows.Forms.TextBox txtWeightMars;
+        private System.Windows.Forms.Button btnCalculate;
     }
 }
 
