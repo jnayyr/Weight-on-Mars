0ea3552fcae14f03f93e37f27ece4a0cbb4a6b20
Ajay Nair <AJ4942BO@go.minnstate.edu>
2020-04-21 16:23:56 -0500

    Refactored button calculate and added additional exception handling for txtObject.

--- Weight on Mars/Form1.Designer.cs
+++ Weight on Mars/Form1.Designer.cs
@@ -33,12 +33,14 @@
             this.txtWeightEarth = new System.Windows.Forms.TextBox();
             this.txtWeightMars = new System.Windows.Forms.TextBox();
             this.btnCalculate = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtObject = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(77, 66);
+            this.label1.Location = new System.Drawing.Point(77, 98);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(166, 25);
             this.label1.TabIndex = 0;
@@ -55,16 +57,16 @@
             // 
             // txtWeightEarth
             // 
-            this.txtWeightEarth.Location = new System.Drawing.Point(291, 66);
+            this.txtWeightEarth.Location = new System.Drawing.Point(291, 95);
             this.txtWeightEarth.Name = "txtWeightEarth";
-            this.txtWeightEarth.Size = new System.Drawing.Size(144, 31);
+            this.txtWeightEarth.Size = new System.Drawing.Size(198, 31);
             this.txtWeightEarth.TabIndex = 2;
             // 
             // txtWeightMars
             // 
             this.txtWeightMars.Location = new System.Drawing.Point(291, 154);
             this.txtWeightMars.Name = "txtWeightMars";
-            this.txtWeightMars.Size = new System.Drawing.Size(144, 31);
+            this.txtWeightMars.Size = new System.Drawing.Size(198, 31);
             this.txtWeightMars.TabIndex = 3;
             // 
             // btnCalculate
@@ -75,12 +77,32 @@
             this.btnCalculate.TabIndex = 4;
             this.btnCalculate.Text = "Calculate";
             this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(83, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Name of Object";
+            // 
+            // txtObject
+            // 
+            this.txtObject.Location = new System.Drawing.Point(291, 39);
+            this.txtObject.MaxLength = 50;
+            this.txtObject.Name = "txtObject";
+            this.txtObject.Size = new System.Drawing.Size(198, 31);
+            this.txtObject.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(562, 304);
+            this.Controls.Add(this.txtObject);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.btnCalculate);
             this.Controls.Add(this.txtWeightMars);
             this.Controls.Add(this.txtWeightEarth);
@@ -100,6 +122,8 @@
         private System.Windows.Forms.TextBox txtWeightEarth;
         private System.Windows.Forms.TextBox txtWeightMars;
         private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtObject;
     }
 }
 
--- Weight on Mars/Form1.cs
+++ Weight on Mars/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,75 @@ namespace Weight_on_Mars
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private bool ValidatePositiveDouble(string text, out double number, out string errorMessage)
+        {
+            errorMessage = null;
+            number = 0;
+
+            try
+            {
+                number = double.Parse(text);
+
+                if (number >= 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    errorMessage = "Enter a positive number";
+                    return false;
+                }
+            }
+            catch (FormatException)
+            {
+                errorMessage = "Enter a number";
+                return false;
+            }
+            catch (OverflowException)
+            {
+                errorMessage = "Enter a smaller number";
+                return false;            
+            }
+        }
+
+        private bool ValidateName(string text, out string name, out string errorMessage)
+        {
+            errorMessage = null;
+            name = text;
+
+            if(string.IsNullOrEmpty(text))
+            {
+                errorMessage = "Cannot be empty";
+                return false;
+            }    
+            if (text.Length < 2)
+            {
+                errorMessage = "Enter at least two letters";
+                return false;
+            }
+            return true;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (!ValidatePositiveDouble(txtWeightEarth.Text, out double earthWeight, out string weightErrorMessage))
+            {
+                MessageBox.Show(weightErrorMessage, "Earth Weight Error");
+                txtWeightEarth.Focus(); // Move focus to this textbox for the user to correct error
+                return;
+            }
+
+            if(!ValidateName(txtObject.Text, out string objectName, out string nameErrorMessage))
+            {
+                MessageBox.Show(nameErrorMessage, "Object name error");
+                txtObject.Focus(); // Move focus to this textbox so user can correct object name
+                return;
+            }                
+                double conversionFactor = 0.377;
+                double marsWeight = earthWeight * conversionFactor;
+                txtWeightMars.Text = marsWeight.ToString();         
         }
     }
 }
